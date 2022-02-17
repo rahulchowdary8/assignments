ee8ffd8da5e11959cb6c6153dd5c3cc592b356a2
rahulchowdary8 <rahulchow11801533@gmail.com>
2022-02-17 18:01:42 +0530

    springcore - assignment3

---
+++ assignment3/src/main/java/assignment3/BankAccountRepository.java
@@ -0,0 +1,10 @@
+package assignment3;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface BankAccountRepository {
+	
+	public double getBalance(long accountId);
+	public double updateBalance(long accountId, double newBalance);
+}
